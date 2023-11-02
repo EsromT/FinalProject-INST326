ff5518b321a134f5af701d7c7fe955ee9e091125
Prince <115111734+EsromT@users.noreply.github.com>
2023-11-02 19:26:55 -0400

    Add files via upload

---
+++ recommender.py
@@ -0,0 +1,21 @@
+# Simple Content-Based Movie Recommendation Prototype
+
+# Sample dataset
+data = {
+    'Romance': ['The Notebook', 'Titanic', 'Sleepless in Seattle'],
+    'Action': ['Die Hard', 'The Terminator', 'Rambo'],
+    'Comedy': ['The Hangover', 'Superbad', 'Anchorman']
+}
+
+
+def recommend_movies(genre):
+    if genre in data:
+        return data[genre]
+    else:
+        return "Genre not found in the dataset."
+
+
+print(recommend_movies('Romance'))
+print(recommend_movies('Action'))
+print(recommend_movies('Comedy'))
+print(recommend_movies('Drama'))
