bbef90d3a1a14bbe5b6770115be82d38d6cbbfbf
Davidpng3434554 <147752419+Davidpng3434554@users.noreply.github.com>
2023-11-20 14:01:45 -0500

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -4,18 +4,44 @@
 data = {
     'Romance': ['The Notebook', 'Titanic', 'Sleepless in Seattle'],
     'Action': ['Die Hard', 'The Terminator', 'Rambo'],
-    'Comedy': ['The Hangover', 'Superbad', 'Anchorman']
+    'Comedy': ['The Hangover', 'Superbad', 'Anchorman'],
+    'Pop': ['Shape of You', 'Blinding Lights', 'Happy'],
+    'Rock': ['Bohemian Rhapsody', 'Stairway to Heaven', 'Hotel California'],
+    'Podcasts': ['The Joe Rogan Experience', 'Serial', 'TED Talks']
 }
 
+"""
+    Recommends media based on user interests.
 
-def recommend_movies(genre):
-    if genre in data:
-        return data[genre]
+    Parameters:
+    - interests (list): A list of user interests (e.g., ['Romance', 'Pop', 'Podcasts']).
+
+    Returns:
+    - list: A list of recommended media based on the user interests.
+"""
+def recommend_media(interests):
+    recommended_media = []
+
+    for interest in interests:
+        if interest in data:
+            recommended_media.extend(data[interest])
+        else:
+            print(f"Genre '{interest}' not found in the dataset.")
+    
+    return recommended_media
+
+if __name__ == '__main__':
+    print("Welcome to the Media Recommender System!")
+
+    # Get user interests
+    user_input = input("Enter your interests (comma-separated): ")
+    user_interests = [interest.strip() for interest in user_input.split(',')]
+
+    # Get recommendations
+    recommendations = recommend_media(user_interests)
+
+    # Display recommendations
+    if recommendations:
+        print(f"\nRecommended media based on interests: {recommendations}")
     else:
-        return "Genre not found in the dataset."
-
-
-print(recommend_movies('Romance'))
-print(recommend_movies('Action'))
-print(recommend_movies('Comedy'))
-print(recommend_movies('Drama'))
+        print("No recommendations found.")
