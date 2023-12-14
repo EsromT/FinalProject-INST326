ee85ac9c54bb439fb2a9bf6fa1a3b47a50f8c73a
Prince <esrom1625@gmail.com>
2023-12-14 16:17:05 -0500

    Makes top code into method for universal use

--- recommender.py
+++ recommender.py
@@ -1,29 +1,32 @@
 import pandas as pd
 # Simple Content-Based Movie Recommendation Prototype
 # David Onafuwa, Esrom Tesfay, Ricardo Siles-Herrera
+def process_movie_data(csv_path):
+    #Read in CSV file as a dataframe
+    df = pd.read_csv(csv_path)
 
-#Read in CSV file as a dataframe
-df = pd.read_csv("C:\\Users\\esrom\\OneDrive\\Documents\\FinalProject-INST326\\titles.csv")
+    #Sorts the dataframe to only movies
+    df_movies = df[df['type'].str.upper() == 'MOVIE']
 
-#Sorts the dataframe to only movies
-df_movies = df[df['type'].str.upper() == 'MOVIE']
+    #We sample our dataset to the first 100 movies
+    df_sampled_movies = df_movies.head(n=100)
 
-#We sample our dataset to the first 100 movies
-df_sampled_movies = df_movies.head(n=100)
+    #Delete unnecessary columns from our dataset
+    df_further_sampled_movies = df_sampled_movies.drop(columns=['id','runtime','production_countries', 'seasons', 
+    'imdb_id', 'imdb_votes', 'tmdb_popularity', 'description', 'age_certification'])
 
-#Delete unnecessary columns from our dataset
-df_further_sampled_movies = df_sampled_movies.drop(columns=['id','runtime','production_countries', 'seasons', 
-'imdb_id', 'imdb_votes', 'tmdb_popularity', 'description', 'age_certification'])
+    #Used only to import data to a csv file inorder to see entirety of updated dataframe
+    df_further_sampled_movies.to_csv("output.csv", index=False)
 
-#Used only to import data to a csv file inorder to see entirety of updated dataframe
-"""df_further_sampled_movies.to_csv("output.csv", index=False)"""
+    #Resets index of dataset from 1 to 100. Inorder to properly label first 100 movies.
+    df_further_sampled_movies = df_further_sampled_movies.reset_index(drop=True)
+    df_further_sampled_movies.index+=1
 
-#Resets index of dataset from 1 to 100. Inorder to properly label first 100 movies.
-df_further_sampled_movies = df_further_sampled_movies.reset_index(drop=True)
-df_further_sampled_movies.index+=1
+    return df_further_sampled_movies
 
+processed_data = process_movie_data("C:\\Users\\esrom\\OneDrive\\Documents\\FinalProject-INST326\\titles.csv")
+print(processed_data)
 
-"""print(df_further_sampled_movies)"""
 
 class Rate():
     def best_rated_movie(self, movies_data):
@@ -59,5 +62,4 @@ class GenrePicker():
                 return chosen_genre
             else:
                 print("Invalid genre. Please choose from the available genres.")
-    
     
