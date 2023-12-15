8cd481d57e9375e2ccea6f98fb1a21ff1da30361
Prince <115111734+EsromT@users.noreply.github.com>
2023-12-15 08:48:39 -0500

    Added Docstrings

--- Test_recommender.py
+++ Test_recommender.py
@@ -6,22 +6,20 @@ from unittest import mock
 from recommender import process_movie_data, MovieRecommender, GenrePicker
 
 class TestMovieRecommendation(unittest.TestCase):
+    """A test class for Movie Recommendation functions and classes."""
+
     def setUp(self):
-        # Set up data
+        """Set up data for testing."""
         self.sample_data_path = "titles.csv"
 
     def test_process_movie_data(self):
-        # Test process_movie_data function
+        """Test the process_movie_data function."""
         processed_data = process_movie_data(self.sample_data_path)
-
-        # Assert tht processed_data is a DataFrame
         self.assertIsInstance(processed_data, pd.DataFrame)
 
-
     def test_movie_recommender(self):
-        # Test the MovieRecommender class
-
-        # Create sample DataFram for testing
+        """Test the MovieRecommender class."""
+        # Create a sample DataFrame for testing
         sample_data = pd.DataFrame({
             'genres': [['action'], ['drama'], ['war']]
         })
@@ -35,22 +33,20 @@ class TestMovieRecommendation(unittest.TestCase):
         # Assert that the recommended_movie is a DataFrame
         self.assertIsInstance(recommended_movie, pd.Series)
 
-
     def test_genre_picker(self):
-        # Test the GenrePicker class
-      
-        # Create sample list of genres for testing
+        """Test the GenrePicker class."""
+        # Create a sample list of genres for testing
         sample_genres = ['action', 'drama', 'war']
 
         # Initialize GenrePicker with the sample genres
         genre_picker = GenrePicker(sample_genres)
 
         # Test get_genre_choice method
-        with unittest.mock.patch('builtins.input', return_value='Action'):
+        with unittest.mock.patch('builtins.input', return_value='action'):
             user_choice = genre_picker.get_genre_choice()
 
         # Assert that the user_choice is the expected genre
-        self.assertEqual(user_choice, 'Action')
+        self.assertEqual(user_choice, 'action')
 
 if __name__ == '__main__':
     unittest.main()
